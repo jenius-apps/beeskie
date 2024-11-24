699dd7a64f038e21c53b66923fe94967bd04568f
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-11-24 11:24:59 -0800

    Improvements to sign in page

--- src/BlueskyClient.Uwp/Views/SignInPage.xaml.cs
+++ src/BlueskyClient.Uwp/Views/SignInPage.xaml.cs
@@ -2,7 +2,10 @@
 using BlueskyClient.ViewModels;
 using JeniusApps.Common.Telemetry;
 using Microsoft.Extensions.DependencyInjection;
+using Windows.System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 #nullable enable
@@ -24,8 +27,19 @@ public sealed partial class SignInPage : Page
         App.Services.GetRequiredService<ITelemetry>().TrackPageView(nameof(SignInPage));
     }
 
-    private void OnAppPassHelpClicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    private void OnAppPassHelpClicked(object sender, RoutedEventArgs e)
     {
         App.Services.GetRequiredService<ITelemetry>().TrackEvent(TelemetryConstants.AppPasswordHelpClicked);
+    }
+
+    private async void OnPasswordBoxKeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key is VirtualKey.Enter && 
+            ViewModel.AppPasswordInput.Length > 0 &&
+            ViewModel.UserHandleInput.Length > 0)
+        {
+            e.Handled = true;
+            await ViewModel.SignInCommand.ExecuteAsync(null);
+        }
     }
 }
